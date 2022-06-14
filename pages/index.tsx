3687f6d6c19978c7ba23b5b5dd8d5c4bde1a4165
KKsimform <kishan.k@simformsolutions.com>
2022-06-14 14:13:13 +0530

    Intigrated API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Header from "../components/Header";
 
-const Home: NextPage = () => {
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import requests from "../utils/requests";
+
+const Home = () => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -12,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main>
-        {/* Banner */}
+        <Banner />
         <section>
           {/* row */}
           {/* row */}
@@ -27,3 +28,37 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async( ) =>{
+    const [
+        netflixOriginals,
+        trendingNow,
+        topRated,
+        actionMovies,
+        comedyMovies,
+        horrorMovies,
+        romanceMovies,
+        documentaries,
+    ] = await Promise.all([
+        fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+        fetch(requests.fetchTrending).then((res) => res.json()),
+        fetch(requests.fetchTopRated).then((res) => res.json()),
+        fetch(requests.fetchActionMovies).then((res) => res.json()),
+        fetch(requests.fetchComedyMovies).then((res) => res.json()),
+        fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+        fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+        fetch(requests.fetchDocumentaries).then((res) => res.json()),
+    ])
+      return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+}
